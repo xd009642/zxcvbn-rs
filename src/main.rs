4487ac8c7591ae19b79e2f39f93b1e18b6204adb
Daniel McKenna <danielmckenna93@gmail.com>
2017-02-19 20:39:56 +0000

    Initial commit. Created the project and added main.rs to allow for direct running of the code. Also added the data files from the original dropbox repository.

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+extern crate zxcvbn_rs;
+
+use std::env;
+
+
+fn zxcvbn(password: String, user_dictionary: Vec<String>) {
+    println!("Password is {}", password);
+    println!("User provided custom dict: {:?}", user_dictionary);
+}
+
+fn main() {
+    let password : Option<String> = env::args().nth(1);
+    let user_dictionary: Vec<String> = env::args().skip(2).collect();
+    match password
+    {
+        Some(x) => zxcvbn(x, user_dictionary), 
+        None => println!("Must provide a password"),
+    }
+}
