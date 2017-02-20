c0a3beb5d7b391a545a4477e26bb9ad6d559fe3e
Daniel McKenna <danielmckenna93@gmail.com>
2017-02-20 20:13:17 +0000

    Populated data structures. Removed unused error import

--- build.rs
+++ build.rs
@@ -1,34 +1,28 @@
 use std::fs;
 use std::io::prelude::*;
 use std::collections::HashMap;
-use std::error::Error;
 
-//static mut frequencies : HashMap<String, u32> = HashMap::new();
-//mut frequencies : Vec<(String, u32)> = vec!();
-//static mut list : Vec<String> = vec!();
 
-fn parse_data(data: String) {
+
+fn parse_data(data: String, word_list: &mut Vec<String>, 
+              freqs: &mut HashMap<String, u32>) {
     for line in data.lines() {
         let mut l = line.split_whitespace();
         let size = line.split_whitespace().count();
         match size  {
-           2=> {
+            2=> {
                 let freq_string = match l.next() {
-                Some(frqs) => frqs,
+                Some(frqs) => frqs.to_string(),
                 None => continue,
                 };
-                let freq_count = match l.next().unwrap_or("").parse::<u32>(){
-                    Ok(f) => println!("{} : {}", freq_string, f),
+                let count = match l.next().unwrap_or("").parse::<u32>(){
+                    Ok(f) => f,
                     Err(_) => continue,
                 };
-           },
-            //match freq_string.parse::<u32>() {
-            //    Ok(f) => frequencies.insert(l.nth(1).unwrap().to_string(), f),
-            //    Err(_) => continue,
-            //}
-         1 => continue,
-            //list.push(l.nth(0).unwrap().to_string());
-         _ => continue,
+                freqs.insert(freq_string, count);
+            },
+            1 => word_list.push(l.nth(0).unwrap().to_string()),
+            _ => continue,
         }
     }
 }
@@ -38,12 +32,14 @@ fn main() {
     // Data files are either lists or frequency tables. Load all files in data
     // and then identify and parse accordingly and generate code
     println!("Generating source from /data/");
+    let mut words : Vec<String> = Vec::new();
+    let mut frequencies : HashMap<String, u32> = HashMap::new();
+
     for entry in fs::read_dir("./data").unwrap() {
         let dir = match entry {
             Ok(dir) => dir,
             Err(_) => continue,
         };
-        println!("data contains: {:?}", dir.path());
         let mut file = match fs::File::open(dir.path()) {
             Ok(file) => file,
             Err(_) => continue,
@@ -51,9 +47,8 @@ fn main() {
         let mut s = String::new();
         match file.read_to_string(&mut s) {
             Err(_) => continue,
-            Ok(_) => parse_data(s),
+            Ok(_) => parse_data(s, &mut words, &mut frequencies),
         }
     }
-
     panic!("Nope");
 }
