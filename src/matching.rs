a0f9494be7c0d2780d414adfa78d5382bdf0eed2
Daniel McKenna <danielmckenna93@gmail.com>
2017-03-06 21:39:05 +0000

    Implemented first version of the dictionary match. Match data specific to dictionary matcher not yet implemented. Also implement comparisons for the BaseMatch to allow for sorting.

--- src/matching.rs
+++ src/matching.rs
@@ -1,7 +1,38 @@
 use std::env;
+use std::cmp::Ordering;
 
 include!(concat!(env!("OUT_DIR"), "/adjacency_data.rs"));
 include!(concat!(env!("OUT_DIR"), "/frequency_data.rs"));
+
+#[derive(Eq)]
+struct BaseMatch
+{
+    pattern: String,
+    start: usize,
+    end: usize,
+    token: String,
+}
+
+impl Ord for BaseMatch {
+    fn cmp(&self, other: &BaseMatch) -> Ordering {
+        let t1 = (self.start, self.end);
+        let t2 = (other.start, other.end);
+        t1.cmp(&t2)
+    }
+}
+
+impl PartialOrd for BaseMatch {
+    fn partial_cmp(&self, other: &BaseMatch) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl PartialEq for BaseMatch {
+    fn eq(&self, other: &BaseMatch) -> bool {
+        (self.start, self.end) == (other.start, other.end)
+    }
+}
+
 
 
 /// Matches the password against every matcher returning the matches
@@ -24,6 +55,22 @@ fn master_dictionary_match(password: &str, matches: &mut Vec<&str>) {
 }
 
 fn dictionary_match<'a>(password: &str, matches: &mut Vec<&str>, 
-                        dictionary: &[&'static str]) {
-    println!("NOT IMPLEMENTED");
+                        dictionary: &[&'static str]) -> Vec<BaseMatch> {
+
+    let mut matches: Vec<BaseMatch> = Vec::new();
+
+    let lower = password.to_lowercase();
+    for i in 0..password.len() {
+        for j in i..password.len()+1 {
+            let slice = &password[i..j];
+            if let Some(pass) = dictionary.iter().position(|&x| x==slice) {
+                matches.push(BaseMatch { pattern:String::from("Dictionary"),
+                                         start:i, end:j, 
+                                         token:slice.to_string()});
+            }
+        }
+    }
+
+    matches.sort();
+    matches
 }
