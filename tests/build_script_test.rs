a894622998e26bb64f57a4ba82f018514cffd8d4
Daniel McKenna <danielmckenna93@gmail.com>
2017-03-09 16:57:10 +0000

    Fixed error in code generation test.
    Fixed the error that caused the test to hang indefinitely.

--- tests/build_script_test.rs
+++ tests/build_script_test.rs
@@ -49,24 +49,24 @@ fn no_duplicates() {
             words.append(&mut get_words(s));
         }
     }
-    let mut repeated_words = words.iter()
-        .filter(|x| words.iter().position(|y| y==*x).unwrap() != 
-                    words.iter().rposition(|y| y==*x).unwrap())
-        .collect::<Vec<_>>();
-    repeated_words.sort();
-    repeated_words.dedup();
-    // Now we have all words that occur more than once!
+    words.sort();
+    words.dedup();
     
     let dicts = vec![FEMALE_NAMES, MALE_NAMES, SURNAMES, PASSWORDS,
                      ENGLISH_WIKIPEDIA, US_TV_AND_FILM];
-
-    for word in repeated_words {
+    let dict_names=vec!["Female names", "Male names", "Surnames", "Passwords",
+                        "Wikipedia", "TV and film"];
+    
+    for word in words {
+        let mut appearances:Vec<&str> = Vec::new();
         let mut count:usize=0;
-        for d in dicts.iter() {
+        for (i, d) in dicts.iter().enumerate() {
             if d.to_vec().contains(&word.as_ref()) {
                 count += 1;
+                appearances.push(dict_names[i]);
             }
         }
-        assert_eq!(1, count);
+        assert!(count<2, "Failed: {} appears {} times\n\
+                Appears in: {:?}", word, count, appearances);
     }
 }
